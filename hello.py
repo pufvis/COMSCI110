fce6d28943d667298a4cb46db969d941557588ca
Melea Laty <meleala2@gmail.com>
2025-03-29 21:57:14 -0700

    Create a file

---
+++ hello.py
@@ -0,0 +1,14 @@
+
+[C[A[A[B[A[C[C[Ckkk
+[A[D[C[C[C[C[C[C[C[C[C[C[Cls
+[A[C[Cdir
+[A[Chelp
+[[C[C[C$ ls
+[A[C[C[Cgit add hello.py
+
+cd
+
+$ help
+[[C[C[C[C[A[C[C[C[C[C[C[C[C[C[cd COMSCI110
+[C0[C[C[C[C[C[C[C[git clone 
+[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[Chttps://github.com/pufvis/COMSCI110
